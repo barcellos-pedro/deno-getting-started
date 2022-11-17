bb7bc45e5feaa39deb0c432c3723ddfafd2e74a4
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-11-16 23:47:10 -0300

    Add npm modules example

---
+++ npm-modules.ts
@@ -0,0 +1,9 @@
+import express from "npm:express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => console.log("app running on port 3000"));
