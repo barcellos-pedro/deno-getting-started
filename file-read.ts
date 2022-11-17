659787f5123d870044e65a8892d82678f289d21f
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-11-17 14:02:04 -0300

    Add file system examples

---
+++ file-read.ts
@@ -0,0 +1,11 @@
+import { serve } from "https://deno.land/std@0.165.0/http/server.ts";
+
+serve(async (_request: Request): Promise<Response> => {
+  const content = await Deno.readTextFile("./assets/hello.txt");
+
+  return new Response(content, {
+    headers: {
+      "content-type": "text/plain",
+    },
+  });
+});
---
+++ video-stream.ts
@@ -0,0 +1,16 @@
+import { serve } from "https://deno.land/std@0.165.0/http/server.ts";
+
+/**
+ * Serve a video stream.
+ * Use file as stream to avoid loading the whole file into memory.
+ */
+serve(async (_request: Request): Promise<Response> => {
+  const file = await Deno.open("./assets/video.mp4", { read: true });
+  const fileStream = file.readable;
+
+  return new Response(fileStream, {
+    headers: {
+      "content-type": "video/mp4",
+    },
+  });
+});
