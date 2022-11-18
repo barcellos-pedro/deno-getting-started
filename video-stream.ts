a7bee0e502833f9ba27fed3ec0c17e5efa47000f
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-11-18 18:05:44 -0300

    Add file size info

--- video-stream.ts
+++ video-stream.ts
@@ -6,11 +6,13 @@ import { serve } from "https://deno.land/std@0.165.0/http/server.ts";
  */
 serve(async (_request: Request): Promise<Response> => {
   const file = await Deno.open("./assets/video.mp4", { read: true });
+  const fileInfo = await file.stat();
   const fileStream = file.readable;
 
   return new Response(fileStream, {
     headers: {
       "content-type": "video/mp4",
+      "content-length": String(fileInfo.size),
     },
   });
 });
